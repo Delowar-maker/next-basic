6de39ee4d46cea37c127f1d1544469dc1bd6a7f1
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-08-10 02:58:35 +0600

    error

---
+++ app/dashboard/error.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { createUser } from "@/app/actions";
+import { useActionState } from "react";
+
+const initialState = {
+    message: "",
+};
+
+export function Signup() {
+    const [state, formAction] = useActionState(createUser, initialState);
+
+    return (
+        <form action={formAction}>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" required />
+            {/* ... */}
+            <p aria-live="polite">{state?.message}</p>
+            <button>Sign up</button>
+        </form>
+    );
+}
+
+// project structured
+
+// <RootLayout>
+//     <Suspense fallback={<DashboardLoding/>}>
+//     <DashboardLayout>
+//          <ErrorBoundary>
+//         <Dashboard/>
+//     </DashboardLayout>
+//           </ErrorBoundary>
+//     </Suspense>
+// </RootLayout>
---
+++ app/dashboard/loading.js
@@ -0,0 +1,8 @@
+
+export default function DashboardLoding() {
+    return (
+        <div>
+            <h1>Dashboard segment Loding .....</h1>
+        </div>
+    )
+}
